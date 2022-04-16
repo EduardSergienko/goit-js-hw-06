63b24a2006d1a069a554cf7e8aeb9170c0a714da
EduardSergienko <katleraster@gmail.com>
2022-04-16 10:52:29 +0300

    Виправив завдання

--- js/task-01.js
+++ js/task-01.js
@@ -3,35 +3,10 @@ const categoriesEl = document.querySelector('#categories');
 console.log(`Number of categories: ${categoriesEl.children.length}`);
 
 
-// Находим первый элемент в списке категорий
-const firstCatEl = categoriesEl.firstElementChild
+const categoryItems = document.querySelectorAll('.item');
 
-// Выводим название категории
-const firstCatElTitle = firstCatEl.firstElementChild.textContent
-console.log(`Category: ${firstCatElTitle}`);
 
-// Ищем список подкатегорий и выводим их количество
-const firstCatSubEl = firstCatEl.lastElementChild
-console.log(`Elements: ${firstCatSubEl.children.length}`);
-
-// Находим второй элемент в списке категорий
-const secondCatEl = firstCatEl.nextElementSibling
-
-// Выводим название категории
-const secondCatElTitle = secondCatEl.firstElementChild.textContent
-console.log(`Category: ${secondCatElTitle}`);
-
-// Ищем список подкатегорий и выводим их количество
-const secondCatSubEl = secondCatEl.lastElementChild
-console.log(`Elements: ${secondCatSubEl.children.length}`);
-
-// Находим третий элемент в списке категорий
-const thirdCatEl = secondCatEl.nextElementSibling
-
-// Выводим название категории
-const thirdCatElTitle = thirdCatEl.firstElementChild.textContent
-console.log(`Category: ${thirdCatElTitle}`);
-
-// Ищем список подкатегорий и выводим их количество
-const thirdCatSubEl = thirdCatEl.lastElementChild
-console.log(`Elements: ${thirdCatSubEl.children.length}`);
+categoryItems.forEach((category) => {
+    console.log(`Category: ${category.firstElementChild.textContent}`);
+    console.log(`Category: ${category.lastElementChild.children.length}`);
+})
